992feef83cbb7ad0c0345596fe30a7d3becf9c95
yasir19 <yasir.habboo@gmail.com>
2021-11-28 22:03:42 -0600

    ready for production

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ var spicalChar = function (){
   return symbols[Math.floor(Math.random()* symbols.length)];
 };
 
-// input function (asking the user for inpu password length, password characters) 
+// input function (asking the user for input password length, password characters) 
 var lengthInput = function (){
-  // promt the user for password length
+    // prompt the user for password length
   var userInput = window.prompt('Enter password length from 8 to 128 characters.');
   userInput = parseInt(userInput);
 //check the user input
 if (userInput >=8 && userInput<=128){
-  window.alert('you select '+ userInput+ ' characters');
+  window.alert('You select '+ userInput+ ' characters');
   return userInput;
   //1-if the user enter less or more than 128 
   //2-alert the user that they need to enter a valid input
@@ -31,16 +31,16 @@ lengthInput();
 }
 };
 
-//asking the user for uppercase alphabet
+//asking the user for uppercase alphabet function
 var upperInput= function(){
   //ask the user if they like to add uppercase 
-  var userUppercaseIn = window.confirm('would you like UPPERCASE characters');
+  var userUppercaseIn = window.confirm('Would you like UPPERCASE characters');
   // check the return value of the user selection
   if (userUppercaseIn){
     //inform the user if the return value is true
     window.alert('you included UPPERCASE');
    return true;
-   //infrom the user if the return value is false
+    //inform the user if the return value is false
   }else{
     window.alert('you choose not to include UPPERCASE characters');
   }
@@ -51,7 +51,7 @@ return false
 //asking the user for lowecase alphabet
 var lowerInput= function(){
     //ask the user if they like to add lowercase
-  var userLowercaseIn = window.confirm('would you like lowercase characters');
+  var userLowercaseIn = window.confirm('Would you like lowercase characters');
    // check the return value of the user selection
   if (userLowercaseIn){
         //inform the user if the return value is true
@@ -64,7 +64,7 @@ var lowerInput= function(){
 return false
 };
 
-//asking the user for number alphabet
+//asking the user for numbers
 var numberInput= function(){
     //ask the user if they like to add numbers
   var userNumberIn = window.confirm('would you like to use numbers');
@@ -96,15 +96,15 @@ function symbolInput() {
   return false;
 };
  
-//create generatedPassword function to store the input function value 
+//create generatePassword function to store the input function value
 var generatedPassword =function() {
   var passwordLength =lengthInput();
   var isUpperCase = upperInput();
   var isLowerCase = lowerInput();
   var isNumbers = numberInput();
   var isSymbols = symbolInput();
-    //ensure the user select at less on password category 
-     while (!isUpperCase && !isLowerCase && !isNumbers && !isSymbols){
+    //ensure the user select at least one password category 
+    if (isUpperCase === false && isLowerCase === false && isNumbers === false && isSymbols ===false){
       window.alert('you did not select any character type, please select at least one type.');
       upperInput();
       lowerInput();
@@ -120,19 +120,19 @@ var generatedPassword =function() {
     };
     return userInput;
     }
-    // function for getting a random element ftom user Input 
+    // function for getting a random element from user Input 
     function randomChar (array){
       var randIndex = Math.floor(Math.random() *array.length); 
       var randEl = array[randIndex];
       return randEl;
     }
-    // function to gnerate password with user input 
+    // function to generate password with user input
     var generatePassword = function() {
       var category = generatedPassword();
 
-      // vbariable to store password 
+      // variable to store password result
       var output =[];
-      //array to store types pf characters that will be included in the password 
+      //array to store types of characters that will be included in the password
       var possCharacters =[];
       // array to store one of each type of selected category to ensure each one will be used 
       var charArray =[];
@@ -144,34 +144,37 @@ var generatedPassword =function() {
         possCharacters = possCharacters.concat(uppercase());
         charArray.push(randomChar(uppercase()));
       }
-       // 1- check if the generatedPassword function has lowerCase then 
-      //2- add them to possArray
-      //3- push it to charArray
+       //1- check if the generatedPassword function has lowerCase then 
+       //2- add them to possArray
+       //3- push it to charArray
       if (category.isLowerCase){
         possCharacters = possCharacters.concat(lowerCase());
         charArray.push(randomChar(lowerCase()));
       }
-       // 1- check if the generatedPassword function has numbers then 
+       //1-c heck if the generatedPassword function has lowerCase then 
       //2- add them to possArray
       //3- push it to charArray
       if (category.isNumbers){
         possCharacters = possCharacters.concat(number());
         charArray.push(randomChar(number()));
       }
-       // 1- check if the generatedPassword function has UpperCase then 
-      //2- add them to possArray
-      //3- push it to charArray
+       //1- check if the generatedPassword function has lowerCase then 
+       //2- add them to possArray
+       //3- push it to charArray
       if (category.isSymbols){
         possCharacters = possCharacters.concat(spicalChar());
         charArray.push(randomChar(spicalChar()));
       }
-       // For loop to iterate over the password length from the options object, selecting random indices from the array of possible characters and concatenating those characters into the result variable
+       //1- For loop to iterate over the password length from the UserInput object
+       //2-selecting random indices from the array of possCharacters
+       //3- contact those characters into the output variable by using the push method
+
        for (var i= 0; i <category.passwordLength; i++ ){
          var charactersEl = randomChar(possCharacters);
          output.push(charactersEl);
        }
-       // Mix in at least one of each guaranteed character in the output
-       for (var i =0; i< charArray.passwordLength; i++ ) {
+       // create a for loop to mix the output characters 
+       for (var i =0; i< charArray.length; i++ ) {
          output[i] =charArray[i];
        }
        // Transform the result into a string and pass into writePassword
